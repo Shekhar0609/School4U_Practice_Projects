8664b603cdc0b7202b2085cb20426d1bd10ff384
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-09-15 00:14:45 +0530

    user lists are added dynamically with js in the user_list_and_search project pratice 1 folder

--- user_list_ans_search_project/practice_1/src/js/script.js
+++ user_list_ans_search_project/practice_1/src/js/script.js
@@ -13,6 +13,12 @@ const users = [
   },
   {
     profileUrl:
+      "https://images.squarespace-cdn.com/content/v1/6204821bfe06b76898b431c5/80221678-0539-4495-8007-0096677e1eca/image00016.jpeg",
+    name: "Guddu Bhaiya",
+    email: "guddupandit@gmail.com",
+  },
+  {
+    profileUrl:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSLgW0m3TnEd8WIBSAej73ovDkRXGBukPkaQ&s",
     name: "munna don",
     email: "munnadudu@gmail.com",
@@ -23,4 +29,58 @@ const users = [
     name: "Guddu Bhaiya",
     email: "guddupandit@gmail.com",
   },
+  {
+    profileUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSLgW0m3TnEd8WIBSAej73ovDkRXGBukPkaQ&s",
+    name: "Munna Bhaiya",
+    email: "Munnababu@gmail.com",
+  },
 ];
+
+const searchInput = document.getElementById("searchInput");
+let userListContainer = document.createElement("ol");
+
+document.addEventListener("DOMContentLoaded", function () {
+  userListContainer.id = "userListContainer";
+  userListContainer.className =
+    "w-full flex flex-col gap-3 overflow-y-auto max-h-90";
+  searchInput.insertAdjacentElement("afterend", userListContainer);
+
+  for (let i = 0; i < users.length; i++) {
+    console.log(users[i].name);
+
+    let userDetailsBox = document.createElement("li");
+    userDetailsBox.id = "userDetailsBox";
+    userDetailsBox.className =
+      "flex items-center gap-3 rounded-xl p-2 cursor-pointer hover:bg-gray-300 duration-300";
+    userListContainer.appendChild(userDetailsBox);
+
+    console.log(userListContainer);
+
+    let userImage = document.createElement("img");
+    userImage.id = "userImage";
+    userImage.src = users[i].profileUrl;
+    userImage.alt = "User Image";
+    userImage.className = "w-13 h-13 rounded-full";
+    userDetailsBox.appendChild(userImage);
+
+    let userInfo = document.createElement("div");
+    userInfo.id = "userInfo";
+    userInfo.className = "flex flex-col";
+    userDetailsBox.appendChild(userInfo);
+
+    let userName = document.createElement("span");
+    userName.id = "userName";
+    userName.className = "capitalize font-[600]";
+    userName.innerText = users[i].name;
+    userInfo.appendChild(userName);
+
+    let userMail = document.createElement("span");
+    userMail.id = "userMail";
+    userMail.className = "lowercase";
+    userMail.innerText = users[i].email;
+    userInfo.appendChild(userMail);
+  }
+});
+
+console.log(userListContainer);
