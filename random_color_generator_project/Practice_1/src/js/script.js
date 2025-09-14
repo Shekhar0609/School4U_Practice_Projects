57900b7248043052644b8c60152f1105449ae9c0
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-09-14 15:49:32 +0530

    added the code explaination line by line in comment in js file in the random color generator project practie 1 folder.

--- random_color_generator_project/Practice_1/src/js/script.js
+++ random_color_generator_project/Practice_1/src/js/script.js
@@ -1,12 +1,13 @@
-const bodyEl = document.querySelector("#body");
-const inputColor = document.getElementById("color-input");
+const bodyEl = document.querySelector("#body"); //body element
+const inputColor = document.getElementById("color-input"); //input color element
 
-const randomColor = document.querySelector("#random-color");
-const colorApply = document.querySelector("#apply-color");
+const randomColor = document.querySelector("#random-color"); //random color button
+const colorApply = document.querySelector("#apply-color"); //apply color button
 
-let outputColorName = document.querySelector("#output-color-name");
+let outputColorName = document.querySelector("#output-color-name"); //output color name element
 
 const colorArray = [
+  //array of colors
   "red",
   "blue",
   "green",
@@ -21,26 +22,31 @@ const colorArray = [
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
-  inputColor.focus();
+  //when document is loaded
+  inputColor.focus(); //focus on input color element
 });
 
 colorApply.addEventListener("click", (color) => {
-  color = inputColor.value;
+  //apply color button click event listener and function
+  color = inputColor.value; //get the value of input color element
 
   if (color === "") {
-    outputColorName.innerText = "please enter color name";
-    outputColorName.classList.add("text-red-800");
-    outputColorName.removeAttribute("style");
+    //if input color is empty
+    outputColorName.innerText = "please enter color name"; //output message
+    outputColorName.classList.add("text-red-800"); // add class to output color name element
+    outputColorName.removeAttribute("style"); //remove style attribute from output color name element
   } else {
-    outputColorName.classList.remove("text-red-800");
-    bodyEl.style.backgroundColor = color;
-    outputColorName.innerText = bodyEl.style.backgroundColor;
+    //if input color is not empty
+    outputColorName.classList.remove("text-red-800"); //remove class from output color name element
+    bodyEl.style.backgroundColor = color; //set background color of body element to input color value
+    outputColorName.innerText = bodyEl.style.backgroundColor; //output the background color of body element
   }
 });
 
 randomColor.addEventListener("click", () => {
-  let randomNumber = Math.floor(Math.random() * colorArray.length);
-  bodyEl.style.backgroundColor = colorArray[randomNumber];
-  outputColorName.innerText = bodyEl.style.backgroundColor;
-  outputColorName.style.color = bodyEl.style.backgroundColor;
+  //random color button click event listener and function
+  let randomNumber = Math.floor(Math.random() * colorArray.length); //generate random number between 0 and length of color array
+  bodyEl.style.backgroundColor = colorArray[randomNumber]; //set background color of body element to random color from color array
+  outputColorName.innerText = bodyEl.style.backgroundColor; //output the background color of body element
+  outputColorName.style.color = bodyEl.style.backgroundColor; //set the text color of output color name element to the background color of body element
 });
