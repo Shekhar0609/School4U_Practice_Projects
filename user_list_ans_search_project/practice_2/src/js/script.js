e04e8da1919fe926eeab7db9a9d3de65c820eaa3
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-09-21 21:53:42 +0530

    user lists are added dynamically with js in the user_list_and_search project pratice 2 folder

--- user_list_ans_search_project/practice_2/src/js/script.js
+++ user_list_ans_search_project/practice_2/src/js/script.js
@@ -44,3 +44,67 @@ const users = [
   },
 ];
 
+let bodyEl = document.querySelector("body");
+
+document.addEventListener("DOMContentLoaded", function () {
+  let containerEl = document.createElement("div");
+  containerEl.id = "container";
+  containerEl.className =
+    "flex flex-col items-center gap-3 p-4 bg-white rounded-lg shadow-lg w-100 min-h-30 mt-10";
+  bodyEl.appendChild(containerEl);
+
+  let headingEl = document.createElement("h2");
+  headingEl.id = "heading";
+  headingEl.className = "text-2xl font-semibold";
+  headingEl.innerText = "User Directory";
+  containerEl.appendChild(headingEl);
+
+  let inputEl = document.createElement("input");
+  inputEl.type = "text";
+  inputEl.placeholder = "Search by name or email...";
+  inputEl.id = "searchInput";
+  inputEl.className = "w-full px-2 py-1";
+  containerEl.appendChild(inputEl);
+  inputEl.focus();
+
+  let userListContainerEl = document.createElement("div");
+  userListContainerEl.id = "userListContainer";
+  userListContainerEl.className =
+    "w-full flex flex-col gap-3 overflow-y-auto max-h-96";
+  containerEl.appendChild(userListContainerEl);
+
+  users.map((user) => {
+    let userName = user.name.toLowerCase();
+    let userMail = user.email.toLowerCase();
+    let profileUrl = user.profileUrl;
+
+    let userDetailsBoxEl = document.createElement("div");
+    userDetailsBoxEl.id = "userDetailsBox";
+    userDetailsBoxEl.className =
+      "flex items-center gap-3 rounded-xl p-2 cursor-pointer hover:bg-gray-300 duration-300";
+    userListContainerEl.appendChild(userDetailsBoxEl);
+
+    let profileImgEl = document.createElement("img");
+    profileImgEl.src = profileUrl;
+    profileImgEl.alt = "profileImg";
+    profileImgEl.className = "w-12 h-12 rounded-full object-cover";
+    userDetailsBoxEl.appendChild(profileImgEl);
+
+    let userInfoEl = document.createElement("div");
+    userInfoEl.id = "userInfo";
+    userInfoEl.className = "flex flex-col";
+    userDetailsBoxEl.appendChild(userInfoEl);
+
+    let userNameEl = document.createElement("span");
+    userNameEl.id = "userName";
+    userNameEl.className = "capitalize font-[600]";
+    userNameEl.innerText = userName;
+    userInfoEl.appendChild(userNameEl);
+
+    let userMailEl = document.createElement("span");
+    userMailEl.id = "userMail";
+    userMailEl.className = "lowercase";
+    userMailEl.innerText = userMail;
+    userInfoEl.appendChild(userMailEl);
+  });
+});
