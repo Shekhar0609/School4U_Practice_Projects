0e01c95bcadf9ce0055a61841048a06f38303241
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-09-28 21:54:07 +0530

    added tailwind css more classes in tags for styling in html file in the simplet to do list project pratice 1 folder

---
+++ simple_to_do_list_project/practice_1/src/js/script.js
@@ -0,0 +1,13 @@
+//if task task height is reached to max height then make it scrollable then editCompleteDeleteBox in be column
+// const task = document.getElementById("task");
+// const editCompleteDeleteBox = document.getElementById("editCompleteDeleteBox");
+// d();
+// function d() {
+//   if (task.scrollHeight > task.clientHeight) {
+//     editCompleteDeleteBox.classList.remove("flex-row");
+//     editCompleteDeleteBox.classList.add("flex-col");
+//   } else {
+//     editCompleteDeleteBox.classList.remove("flex-col");
+//     editCompleteDeleteBox.classList.add("flex-row");
+//   }
+// }
