c805f81144da3df5e82b5116c82fa915f823cbd2
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-10-25 04:10:52 +0530

    solved the qestion04 js files in the in beginner questions folder of 50 questions of js

--- 50 Questions of JS/Beginner Questions/question04/script04.js
+++ 50 Questions of JS/Beginner Questions/question04/script04.js
@@ -1,3 +1,10 @@
 //4. Create a function that takes an array containing both numbers and strings,
 // and returns a new array containing only the string values.
 
+const mixedArray = ["apple", 123, "banana", 45.67, "orange", 89];
+
+let stringArray = mixedArray.filter((stringArr) => {
+  return typeof stringArr === "string";
+});
+
+console.log(stringArray);
