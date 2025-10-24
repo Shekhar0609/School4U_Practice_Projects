ee5738b1b6538e2a800fd9111e87374bc9880bf9
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-10-25 03:52:05 +0530

    created the question02 js files in the in beginner questions folder of 50 questions of js

--- 50 Questions of JS/Beginner Questions/question02/script02.js
+++ 50 Questions of JS/Beginner Questions/question02/script02.js
@@ -1,1 +1,17 @@
 //2. Find the combination of two arrays.
+
+function combinationArray(array1, array2) {
+  let combination = [...array1, ...array2];
+  return combination;
+}
+
+console.log(combinationArray([1, 2, 4], [6, 5, 4]));
+// Output: [1, 2, 4, 6, 5, 4]
+
+console.log(combinationArray(["a", "b", "c"], ["d", "e", "f"]));
+// Output: ['a', 'b', 'c', 'd', 'e', 'f']
+console.log(combinationArray([true, false], [false, false, true]));
+// Output: [true, false, false, false, true]
+console.log(combinationArray([1, [2, 3]], [[4, 5], 6]));
+// Output: [1, [2, 3], [4, 5], 6]
+console.log(combinationArray(new Array(3), new Array(2)));
