631359562c8e03c3d8bfc3eba1426cd83f77fc52
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-10-27 00:27:05 +0530

    solved the qestion06 js files in the in beginner questions folder of 50 questions of js

--- 50 Questions of JS/Beginner Questions/question06/script06.js
+++ 50 Questions of JS/Beginner Questions/question06/script06.js
@@ -1,2 +1,15 @@
 // 6. Write a function that returns the length of a given object (number of keys).
 
+function totalObjLeghtKey(obj) {
+  let ObjectKeyLength = Object.keys(obj);
+  return ObjectKeyLength.length;
+}
+
+totalObjLeghtKey({
+  fname: "jyoti",
+  lname: "Deekonda",
+  age: 55,
+  Nationality: "Indian",
+  isMarried: "Yes",
+});
+// Expected Output: 5
