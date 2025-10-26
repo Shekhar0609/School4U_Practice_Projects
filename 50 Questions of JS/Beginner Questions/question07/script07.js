9f01b69887fafdd95657864ec7c54997a250433b
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-10-27 01:08:45 +0530

    solved the qestion07 js files in the in beginner questions folder of 50 questions of js

--- 50 Questions of JS/Beginner Questions/question07/script07.js
+++ 50 Questions of JS/Beginner Questions/question07/script07.js
@@ -1,2 +1,26 @@
 // 7. In an array of objects filter out those objects which have gender's value male.
 
+let userArrayObject = [
+  {
+    fname: "ram",
+    gender: "male",
+  },
+  { fname: "seeta", gender: "female" },
+  { fname: "laxman", gender: "male" },
+  { fname: "geeta", gender: "female" },
+];
+
+function maleGender(objArr) {
+  let filterArray = objArr.filter((obj) => {
+    return obj.gender === "male";
+  });
+  return filterArray;
+}
+
+maleGender(userArrayObject);
+// Output:
+// [ { fname: 'ram',
+//    gender: 'male' },
+//  { fname: 'laxman',
+//   gender: 'male' } ]
+console.log(maleGender(userArrayObject));
