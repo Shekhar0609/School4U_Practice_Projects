fd64498cba6222be0e53aa7a44db012d9f8ea1ca
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-10-29 01:30:38 +0530

    solved the qestion08 js files in the in beginner questions folder of 50 questions of js

--- 50 Questions of JS/Beginner Questions/question08/script08.js
+++ 50 Questions of JS/Beginner Questions/question08/script08.js
@@ -1,2 +1,15 @@
 // 8. Given an array of strings, return a new array where all strings are in uppercase.
 
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+function convertUpperCase(upperCaseValue) {
+  let newValue = upperCaseValue.map((value) => {
+    return value.toUpperCase();
+  });
+  return newValue;
+}
+
+convertUpperCase(fruits);
+// Output: ["BANANA", "ORANGE", "APPLE", "MANGO"]
+console.log(convertUpperCase(fruits));
+console.log(fruits);
