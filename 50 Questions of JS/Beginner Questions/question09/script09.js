61b8d11130767927f8a42eccefe7e3e1ae08c7f9
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-10-29 12:28:14 +0530

    solved the qestion09 js files in the in beginner questions folder of 50 questions of js

--- 50 Questions of JS/Beginner Questions/question09/script09.js
+++ 50 Questions of JS/Beginner Questions/question09/script09.js
@@ -1,1 +1,11 @@
 // 9. Check if an object is empty (has no keys).
+
+let person = {};
+
+function isObejectEmpty(obj) {
+  let isEmptyKey = Object.keys(obj) < 1 ? "Empty Object" : "Object not Empty";
+  return isEmptyKey;
+}
+
+isObejectEmpty(person);
+console.log(isObejectEmpty(person)); // Output: "Empty Object"
