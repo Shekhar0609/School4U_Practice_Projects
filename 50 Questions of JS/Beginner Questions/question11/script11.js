c4474181f65544755486947ef0fa0adfcc641930
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-10-30 01:23:33 +0530

    solved the qestion11 js files in the in beginner questions folder of 50 questions of js

--- 50 Questions of JS/Beginner Questions/question11/script11.js
+++ 50 Questions of JS/Beginner Questions/question11/script11.js
@@ -1,1 +1,9 @@
 // 11. Convert an array of strings into a single comma-separated string.
+
+let stringArray = ["apple", "banana", "cherry", "red", "green", "blue"];
+
+function singleCommaSeparatedString(str) {
+  return str.join(",");
+}
+singleCommaSeparatedString(stringArray);
+// Output: "apple,banana,cherry,red,green,blue"
