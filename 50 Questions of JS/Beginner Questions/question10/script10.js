2eb9970a83a7ceef434593bcaf75db495d6dc3fd
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-10-30 01:12:36 +0530

    solved the qestion10 with explaintion in comment js files in the in beginner questions folder of 50 questions of js

--- 50 Questions of JS/Beginner Questions/question10/script10.js
+++ 50 Questions of JS/Beginner Questions/question10/script10.js
@@ -1,10 +1,16 @@
 // 10. Create an array of numbers and double each value using .map().
 
+// Example:
 let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
+// Solution:
+// Function to double each number in the array
 function doubleNumbers(numbers) {
-  return numbers.map((number) => number * 2);
+  // Using .map() to create a new array with doubled values
+  return numbers.map((number) => number * 2); // Doubling each number
 }
 
+// Testing the function
+console.log(doubleNumbers(array)); // Output the new array with doubled values
 doubleNumbers(array);
 // Output: [2, 4, 6, 8, 10, 12, 14, 16, 18, 0]
