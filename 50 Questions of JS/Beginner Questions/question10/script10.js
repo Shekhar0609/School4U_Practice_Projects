b91c76a31e1948cab324c36f99bf27b3321e50fb
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-10-30 01:11:24 +0530

    solved the qestion10 js files in the in beginner questions folder of 50 questions of js

--- 50 Questions of JS/Beginner Questions/question10/script10.js
+++ 50 Questions of JS/Beginner Questions/question10/script10.js
@@ -1,1 +1,10 @@
 // 10. Create an array of numbers and double each value using .map().
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+function doubleNumbers(numbers) {
+  return numbers.map((number) => number * 2);
+}
+
+doubleNumbers(array);
+// Output: [2, 4, 6, 8, 10, 12, 14, 16, 18, 0]
