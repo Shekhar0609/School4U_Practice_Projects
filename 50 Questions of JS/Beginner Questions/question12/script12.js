0c6d9df766fdeb1bb83c2c1e06d3ba16a70d6070
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-10-30 01:38:39 +0530

    solved the qestion12 js files in the in beginner questions folder of 50 questions of js

--- 50 Questions of JS/Beginner Questions/question12/script12.js
+++ 50 Questions of JS/Beginner Questions/question12/script12.js
@@ -1,2 +1,14 @@
 // 12. Write a function to flatten a nested array (one level deep).(e.g., [1, [2, 3], 4] → [1, 2, 3, 4])
 
+let nestedArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+function flattenedArr(nestedArr) {
+  return nestedArr.flat();
+}
+
+flattenedArr(nestedArray);
+// Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
