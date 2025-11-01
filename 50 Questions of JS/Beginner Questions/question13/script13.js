61fb5086c92d5808331e922ef41b3e52e47723c7
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-01 17:06:47 +0530

    solved the qestion13 js files in the in beginner questions folder of 50 questions of js

--- 50 Questions of JS/Beginner Questions/question13/script13.js
+++ 50 Questions of JS/Beginner Questions/question13/script13.js
@@ -1,1 +1,10 @@
 // 13. Write a function that checks if all elements in an array are numbers.
+
+let array = ["Alice", 30, true, "Bob", 10, "Charlie", false, 20, true, 30, 40];
+
+function checkNumber(numbers) {
+  return numbers.every((number) => (typeof number === "number" ? true : false));
+}
+
+checkNumber(array);
+// Output: false
