2ff3164fd7790f0f12c8d8eb60692bc31d21fe03
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-02 00:53:52 +0530

    solved the qestion14 js files in the in beginner questions folder of 50 questions of js

--- 50 Questions of JS/Beginner Questions/question14/script14.js
+++ 50 Questions of JS/Beginner Questions/question14/script14.js
@@ -1,3 +1,16 @@
 // 14. Build a simple isPrime() function to check if a number is prime.
 
+function isPrime(num) {
+  if (num <= 1) return false; 
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false; 
+    }
+  }
+  return true; 
+}
 
+isPrime(11); // true
+isPrime(4); // false
+
+console.log(isPrime(3), isPrime(4));
