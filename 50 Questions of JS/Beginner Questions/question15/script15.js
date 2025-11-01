33db66fa0a87ff149b398f3a54e3f9ea9e819a7a
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-02 01:20:19 +0530

    solved the qestion15 with explaintion in comment js files in the in beginner questions folder of 50 questions of js

--- 50 Questions of JS/Beginner Questions/question15/script15.js
+++ 50 Questions of JS/Beginner Questions/question15/script15.js
@@ -1,10 +1,13 @@
 // 15. Create a function that removes duplicate values from an array.
 
+//  Example:
 let array = [11, 2, 2, 3, 4, 4, 5, 5, 56, 33];
 
+//function to remove duplicate values
 function removeDuplicateValue(duplicateValues) {
   return duplicateValues.filter(
-    (value, index) => array.indexOf(value) === index
+    // filter method to filter out duplicate values
+    (value, index) => array.indexOf(value) === index // check if the index of the value is the same as the current index
   );
 }
 
@@ -13,11 +16,14 @@ removeDuplicateValue(array);
 
 console.log(removeDuplicateValue(array));
 
+// Another Approach using Set
 let newArray = [11, 2, 2, 3, 4, 4, 5, 5, 56, 33];
 
+//function to remove duplicate values using Set
 function removeDuplicateValue2(Values) {
-  return [...new Set(Values)];
+  // Set only stores unique values
+  return [...new Set(Values)]; // spread operator to convert Set back to array
 }
-
 removeDuplicateValue2(newArray);
+// Output: [11, 2, 3, 4, 5, 56, 33]
 console.log(removeDuplicateValue2(newArray));
