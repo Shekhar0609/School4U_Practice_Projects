7b0a9a5860dc49e89bfafa629ed73a5d0a64cf89
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-01 17:09:41 +0530

    solved the qestion13 with explaintion in comment js files in the in beginner questions folder of 50 questions of js

--- 50 Questions of JS/Beginner Questions/question13/script13.js
+++ 50 Questions of JS/Beginner Questions/question13/script13.js
@@ -1,8 +1,12 @@
 // 13. Write a function that checks if all elements in an array are numbers.
 
+// Sample Input:
 let array = ["Alice", 30, true, "Bob", 10, "Charlie", false, 20, true, 30, 40];
 
+// Sample Output: false
 function checkNumber(numbers) {
+  // Your code here
+  // Using the every() method to check if all elements are numbers
   return numbers.every((number) => (typeof number === "number" ? true : false));
 }
 
