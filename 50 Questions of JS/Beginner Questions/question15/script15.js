b90ddc03422c6fb2e8a45822396b9886c46ff6a9
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-02 01:18:25 +0530

    solved the qestion15 js files in the in beginner questions folder of 50 questions of js

--- 50 Questions of JS/Beginner Questions/question15/script15.js
+++ 50 Questions of JS/Beginner Questions/question15/script15.js
@@ -1,1 +1,23 @@
 // 15. Create a function that removes duplicate values from an array.
+
+let array = [11, 2, 2, 3, 4, 4, 5, 5, 56, 33];
+
+function removeDuplicateValue(duplicateValues) {
+  return duplicateValues.filter(
+    (value, index) => array.indexOf(value) === index
+  );
+}
+
+removeDuplicateValue(array);
+// Output: [11, 2, 3, 4, 5, 56, 33]
+
+console.log(removeDuplicateValue(array));
+
+let newArray = [11, 2, 2, 3, 4, 4, 5, 5, 56, 33];
+
+function removeDuplicateValue2(Values) {
+  return [...new Set(Values)];
+}
+
+removeDuplicateValue2(newArray);
+console.log(removeDuplicateValue2(newArray));
