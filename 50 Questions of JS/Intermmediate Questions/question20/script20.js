ffcc201b3f525d970365589e5a9b82daba5158fa
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-04 02:16:02 +0530

    solved the qestion20 js files in the in intermmediate questions folder of 50 questions of js

--- 50 Questions of JS/Intermmediate Questions/question20/script20.js
+++ 50 Questions of JS/Intermmediate Questions/question20/script20.js
@@ -1,1 +1,12 @@
 // 20. What is the difference between slice and splice?
+
+const fruits = ["apple", "banana", "cherry", "date"];
+
+console.log(fruits.slice(1, 3));
+
+console.log(fruits);
+
+fruits.splice(1, 2, "blueberry", "dragonfruit");
+console.log(fruits);
+
+console.log(fruits);
