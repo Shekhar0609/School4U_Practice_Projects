44279a7f4868f9ab21ad850e7bdd1f00da7ea892
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-05 03:04:44 +0530

    solved the question21 with explaintion in comment js files in the in intermmediate questions folder of 50 questions of js

--- 50 Questions of JS/Intermmediate Questions/question21/script21.js
+++ 50 Questions of JS/Intermmediate Questions/question21/script21.js
@@ -1,12 +1,17 @@
 // 21. Create a function that reverse each word of a given sentence.
 // E.g., Mai hun manas → sanam nuh iam
 
-let sentance = "APURVIKA";
+// Solution:
+// Define the sentence to be reversed
+let sentance = "Mai hun manas";
 
+// Function to reverse each word in the sentence
 function reverseFunction(words) {
-  let wordArray = Array.from(words);
-  return wordArray.reverse().join("");
+  // words = "Mai hun manas"
+  let wordArray = Array.from(words); // wordArray = ['M', 'a', 'i', ' ', 'h', 'u', 'n', ' ', 'm', 'a', 'n', 'a', 's']
+  return wordArray.reverse().join(""); // sanam nuh iam
 }
 
+// Call the function and log the result
 reverseFunction(sentance); // sanam nuh iam
-console.log(reverseFunction(sentance));
+console.log(reverseFunction(sentance)); // sanam nuh iam
