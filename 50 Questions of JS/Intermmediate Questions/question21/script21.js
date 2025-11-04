ad98e9bdc6a760de01071d093ebcebc1e08c7adc
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-05 03:03:18 +0530

    solved the qestion21 js files in the in intermmediate questions folder of 50 questions of js

--- 50 Questions of JS/Intermmediate Questions/question21/script21.js
+++ 50 Questions of JS/Intermmediate Questions/question21/script21.js
@@ -1,2 +1,12 @@
-// 21. Create a function that reverse each word of a given sentence. 
+// 21. Create a function that reverse each word of a given sentence.
 // E.g., Mai hun manas → sanam nuh iam
+
+let sentance = "APURVIKA";
+
+function reverseFunction(words) {
+  let wordArray = Array.from(words);
+  return wordArray.reverse().join("");
+}
+
+reverseFunction(sentance); // sanam nuh iam
+console.log(reverseFunction(sentance));
