c5c6081b568e1b40b24cd6ae36a03f88fc8ea4c1
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-06 02:22:27 +0530

    solved the qestion22 js files in the in intermmediate questions folder of 50 questions of js

--- 50 Questions of JS/Intermmediate Questions/question22/script22.js
+++ 50 Questions of JS/Intermmediate Questions/question22/script22.js
@@ -1,1 +1,16 @@
 // 22. In an array of numbers and strings, only add those numbers which are not strings.
+
+let stringNumberArray = [1, "2", 3, "4", 5, "hello", 6, "world"];
+
+let sum = 0;
+
+function numberArray(nums) {
+  nums.forEach((num) => {
+    if (typeof num == "number") {
+      sum += num;
+    }
+  });
+}
+
+numberArray(stringNumberArray);
+console.log(sum);
