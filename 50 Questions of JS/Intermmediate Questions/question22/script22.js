cbc892063a851c62ad2b394080468a55512d410b
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-06 02:24:36 +0530

    solved the question22 with explaintion in comment js files in the in intermmediate questions folder of 50 questions of js

--- 50 Questions of JS/Intermmediate Questions/question22/script22.js
+++ 50 Questions of JS/Intermmediate Questions/question22/script22.js
@@ -1,16 +1,27 @@
 // 22. In an array of numbers and strings, only add those numbers which are not strings.
 
+// Example Input: [1, "2", 3, "4", 5, "hello", 6, "world"]
+// Example Output: 15 (1 + 3 + 5 + 6)
 let stringNumberArray = [1, "2", 3, "4", 5, "hello", 6, "world"];
 
+// Initialize sum variable to store the sum of numbers
 let sum = 0;
 
+// Function to iterate through the array and add only numbers to sum and ignore strings
+// nums is the parameter representing the input array
 function numberArray(nums) {
+  // Iterate through each element in the array
   nums.forEach((num) => {
     if (typeof num == "number") {
-      sum += num;
+      // Check if the element is of type number
+      // If it is a number, add it to sumF
+      sum += num; // sum = sum + num
     }
   });
 }
 
+// Call the function with the provided array
 numberArray(stringNumberArray);
+// Print the final sum of numbers
 console.log(sum);
+// Expected Output: 15
