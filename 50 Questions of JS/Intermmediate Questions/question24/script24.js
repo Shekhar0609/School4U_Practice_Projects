262118f4dd847c1b72d3d77e81a2ed1d4fd62ae5
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-09 00:05:43 +0530

    solved the question24 with explaintion in comment js files in the in intermmediate questions folder of 50 questions of js

--- 50 Questions of JS/Intermmediate Questions/question24/script24.js
+++ 50 Questions of JS/Intermmediate Questions/question24/script24.js
@@ -1,11 +1,15 @@
 // 24. Write a JavaScript function that reverse a number.
 
+// Example x = 1234567890;
 function reverseNumber(numb) {
+  // function starts here
+  // convert number to string, split into array, reverse array, join back to string, convert back to number
   let reverseString = numb.toString().split("").reverse();
-  return Number(reverseString.join(""));
+  return Number(reverseString.join("")); // return the reversed number
 }
 
+// call the function
 reverseNumber(1234567890);
 // Output: 0987654321
 
-console.log(reverseNumber(1234567890));
+console.log(reverseNumber(1234567890)); // Output: 987654321
