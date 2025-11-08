7d79ea4b1639143a9a95f2fc45e62f99aa442ba1
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-09 02:24:31 +0530

    solved the qestion27 js files in the in intermmediate questions folder of 50 questions of js

--- 50 Questions of JS/Intermmediate Questions/question27/script27.js
+++ 50 Questions of JS/Intermmediate Questions/question27/script27.js
@@ -1,3 +1,47 @@
 /* 27. Write a javascript function to get the first element of an array. Passing a parameter 'n' will
 return the first 'n' elements of the array.*/
 
+function returnNofArrays(array, nNumnber) {
+  if (!nNumnber) {
+    return array[0];
+  } else if (array.length <= nNumnber) {
+    return console.log("exceeding the no of array length");
+  } else {
+    return array.slice(0, nNumnber);
+  }
+}
+
+returnNofArrays([4, 5, 6, 7, 8]);
+console.log(returnNofArrays([4, 5, 6, 7, 8]));
+
+returnNofArrays([4, 5, 6, 7, 8], 7);
+console.log(returnNofArrays([4, 5, 6, 7, 8], 7));
+
+returnNofArrays([4, 5, 6, 7, 8], 4);
+console.log(returnNofArrays([4, 5, 6, 7, 8], 4));
+
+function returnNofArrays2(array, nNumnber) {
+  if (!nNumnber) {
+    return array[0];
+  } else if (nNumnber > array.length) {
+    return console.log("exceeding the no of array length");
+  }
+
+  let newArray = [];
+
+  array.forEach((arr, ind) => {
+    if (ind < nNumnber) {
+      return newArray.push(arr);
+    }
+  });
+  return newArray;
+}
+
+returnNofArrays2([4, 5, 6, 7, 8]);
+console.log(returnNofArrays2([4, 5, 6, 7, 8]));
+
+returnNofArrays2([4, 5, 6, 7, 8], 8);
+console.log(returnNofArrays2([4, 5, 6, 7, 8], 8));
+
+returnNofArrays2([4, 5, 6, 7, 8], 4);
+console.log(returnNofArrays2([4, 5, 6, 7, 8], 4));
