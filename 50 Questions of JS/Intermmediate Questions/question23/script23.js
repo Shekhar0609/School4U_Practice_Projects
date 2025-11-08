71eb55d9730ee5ecc99864a7c32634d32fe97103
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-08 17:41:07 +0530

    solved the qestion23 js files in the in intermmediate questions folder of 50 questions of js

--- 50 Questions of JS/Intermmediate Questions/question23/script23.js
+++ 50 Questions of JS/Intermmediate Questions/question23/script23.js
@@ -1,1 +1,14 @@
 // 23. How would you check if a number is an integer?
+
+function isInteger(num) {
+  return Number.isInteger(num);
+}
+
+console.log(typeof 23, 23, isInteger(23)); // true
+console.log(typeof 3.14, 3.14, isInteger(3.14)); // false
+console.log(typeof -7, -7, isInteger(-7)); // true
+console.log(typeof 0, 0, isInteger(0)); // true
+console.log(typeof "10", "10", isInteger("10")); // false
+console.log(NaN, isInteger(NaN)); // false
+console.log(typeof Infinity, Infinity, isInteger(Infinity)); // false
+console.log(typeof -Infinity, -Infinity, isInteger(-Infinity)); // false
