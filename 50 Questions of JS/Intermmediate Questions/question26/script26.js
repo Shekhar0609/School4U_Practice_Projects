60b4bfdc30095815673a3ef627c7217ec4087cbc
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-09 01:21:40 +0530

    solved the qestion26 js files in the in intermmediate questions folder of 50 questions of js

--- 50 Questions of JS/Intermmediate Questions/question26/script26.js
+++ 50 Questions of JS/Intermmediate Questions/question26/script26.js
@@ -1,3 +1,32 @@
 /* 26. Write a Javascript function that accepts a string as a parameter and conversts the first
 letter of each word of the string in upper case.*/
 
+function firstLetterToUpperCase(words) {
+  let wordsArray = words.split(" ");
+  let firstLetterToUpperCase = wordsArray
+    .map((word) => {
+      let initialLetterUperCase = word[0].toUpperCase();
+      initialLetterUperCase = word.replace(word[0], initialLetterUperCase);
+      return initialLetterUperCase;
+    })
+    .join(" ");
+  return firstLetterToUpperCase;
+}
+
+firstLetterToUpperCase("i am shekhar Narsimulu deekonda");
+console.log(firstLetterToUpperCase("i am shekhar Narsimulu deekonda"));
+
+function firstLetterToUpperCase2(words) {
+  let wordsArray = words.split(" ");
+  let firstLetterToUpperCase = wordsArray
+    .map((word) => {
+      let initialLetterUperCase = word.charAt(0).toUpperCase();
+      let remainingLetters = word.slice(1);
+      return initialLetterUperCase + remainingLetters;
+    })
+    .join(" ");
+  return firstLetterToUpperCase;
+}
+
+firstLetterToUpperCase2("i am shekhar Narsimulu deekonda");
+console.log(firstLetterToUpperCase2("i am shekhar Narsimulu deekonda"));
