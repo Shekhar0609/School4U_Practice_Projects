41e39f1c1139f5e254df0a57dea62908d134a45f
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-09 00:04:14 +0530

    solved the qestion24 js files in the in intermmediate questions folder of 50 questions of js

--- 50 Questions of JS/Intermmediate Questions/question24/script24.js
+++ 50 Questions of JS/Intermmediate Questions/question24/script24.js
@@ -1,1 +1,11 @@
 // 24. Write a JavaScript function that reverse a number.
+
+function reverseNumber(numb) {
+  let reverseString = numb.toString().split("").reverse();
+  return Number(reverseString.join(""));
+}
+
+reverseNumber(1234567890);
+// Output: 0987654321
+
+console.log(reverseNumber(1234567890));
