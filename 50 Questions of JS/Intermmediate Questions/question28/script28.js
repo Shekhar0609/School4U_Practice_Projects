63daa46b468a53178391b443a57fd4f09192acb3
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-09 04:06:36 +0530

    solved the qestion28 js files in the in intermmediate questions folder of 50 questions of js

--- 50 Questions of JS/Intermmediate Questions/question28/script28.js
+++ 50 Questions of JS/Intermmediate Questions/question28/script28.js
@@ -1,3 +1,40 @@
 /* 28. Write a javascript function to get the number of occurrences of each letter in specified
 string.*/
 
+let fname = "shekhar narsimulu deekonda";
+
+function countLetters(letter) {
+  let countLetterObj = {};
+
+  let letterArray = letter.split("");
+
+  letterArray.forEach((letter) => {
+    if (countLetterObj.hasOwnProperty(letter)) {
+      countLetterObj[letter]++;
+    } else {
+      countLetterObj[letter] = 1;
+    }
+  });
+  console.log(countLetterObj);
+}
+
+countLetters(fname);
+
+function countLetters2(letter) {
+  let countLetterObj2 = {};
+
+  let letterArray = letter.split("");
+
+  for (let index = 0; index < letterArray.length; index++) {
+    let currentLetter = letterArray[index];
+
+    if (countLetterObj2.hasOwnProperty(currentLetter)) {
+      countLetterObj2[currentLetter]++;
+    } else {
+      countLetterObj2[currentLetter] = 1;
+    }
+  }
+  console.log(countLetterObj2);
+}
+
+countLetters2(fname);
