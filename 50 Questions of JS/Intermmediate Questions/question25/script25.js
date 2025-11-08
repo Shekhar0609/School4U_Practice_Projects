d3c255d9755e9652dabef379bfdfb5729ff7301d
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-09 00:32:22 +0530

    solved the qestion25 js files in the in intermmediate questions folder of 50 questions of js

--- 50 Questions of JS/Intermmediate Questions/question25/script25.js
+++ 50 Questions of JS/Intermmediate Questions/question25/script25.js
@@ -1,1 +1,7 @@
 // 25. Write a javascript function that returns a passed string with letters in alphabetical order.
+
+function alpabeticalOrder(words) {
+  return words.split(" ").sort().join(" ");
+}
+
+alpabeticalOrder("shekhar Narsimulu deekonda");
