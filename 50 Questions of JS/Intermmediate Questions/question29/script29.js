bc84c66079896084f70084989a2fcb3a7c000eeb
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-09 04:22:31 +0530

    solved the qestion29 js files in the in intermmediate questions folder of 50 questions of js

--- 50 Questions of JS/Intermmediate Questions/question29/script29.js
+++ 50 Questions of JS/Intermmediate Questions/question29/script29.js
@@ -1,1 +1,38 @@
 /* 29. write a javascript program to find the most frequent item of an array.*/
+
+let numberArray = [1,2,3,4,2,44,52,4,2,2,7.65,545,0.4]; 
+
+function countLetters(number) {
+  let countNumberObj = {};
+
+  number.forEach((num) => {
+    if (countNumberObj.hasOwnProperty(num)) {
+      countNumberObj[num]++; 
+    } else {
+      countNumberObj[num] = 1;
+    }
+  });
+  return countNumberObj;
+}
+
+countLetters(numberArray);
+console.log(countLetters(numberArray));
+
+
+function countLetters2(number) {
+  let countNumberObj2 = {};
+
+  for (let index = 0; index < number.length; index++) { 
+    let currentNumber = number[index];
+
+    if (countNumberObj2.hasOwnProperty(currentNumber)) { 
+      countNumberObj2[currentNumber]++;
+    } else {
+      countNumberObj2[currentNumber] = 1;
+    }
+  }
+  return countNumberObj2;
+}
+
+countLetters2(numberArray);
+console.log(countLetters2(numberArray));
