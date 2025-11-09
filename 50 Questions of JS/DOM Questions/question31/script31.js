b87328592bb4244c36d2f9c6a1eec70b634aea5d
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-09 22:37:02 +0530

    solved the qestion31 in the in DOM questions folder of 50 questions of js

--- 50 Questions of JS/DOM Questions/question31/script31.js
+++ 50 Questions of JS/DOM Questions/question31/script31.js
@@ -1,4 +1,10 @@
 /* 31. How can you update the DOM based on user input in real-time (e.g., live preview of a
 form)?*/
 
+const inputTextEl = document.querySelector("#inputText");
 
+const outputEl = document.querySelector("#output");
+
+inputTextEl.addEventListener("input", (e) => {
+  return outputEl.innerText = e.target.value
+});
