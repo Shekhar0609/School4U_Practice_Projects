761b8ee1c7cdc477be99fc0e66c21d6420eec11d
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-09 06:07:04 +0530

    solved the qestion30 js files in the in intermmediate questions folder of 50 questions of js

--- 50 Questions of JS/Intermmediate Questions/question30/script30.js
+++ 50 Questions of JS/Intermmediate Questions/question30/script30.js
@@ -1,1 +1,10 @@
 /* 30. write a javascript program to shuffle an array.*/
+
+let array = [14, 25, 36, 43, 35, 6, 7, 8, 9];
+
+for (let index in array) {
+  let randomIndex = Math.floor(Math.random() * array.length);
+
+  [array[randomIndex], array[index]] = [array[index], array[randomIndex]];
+}
+console.log(array);
