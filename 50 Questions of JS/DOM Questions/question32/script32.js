fac60f5dd2a41b4dd0ecddd5338525d14510f466
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-13 03:52:45 +0530

    solved the qestion32 js files in the in DOM questions folder of 50 questions of js

--- 50 Questions of JS/DOM Questions/question32/script32.js
+++ 50 Questions of JS/DOM Questions/question32/script32.js
@@ -1,2 +1,35 @@
 /* 32. How would you handle form validation in real time and show error messages
 dynamically?*/
+
+const userUserName = document.querySelector("#userUserName");
+const userPassword = document.querySelector("#userPassword");
+const showPassword = document.querySelector("#showPassword");
+const alertMessege = document.querySelector(".alert");
+const formEl = document.querySelector("#form");
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let userUserNameValue = userUserName.value;
+  let userPasswordValue = userPassword.value;
+
+  if (userUserNameValue !== userPasswordValue) {
+    alertMessege.innerText = `Username and Password is incorrect`;
+    alertMessege.style.color = "red";
+    userUserName.value = "";
+    userPassword.value = "";
+  } else {
+    alertMessege.innerText = `Username and Password is correct`;
+    alertMessege.style.color = "lightgreen";
+    userUserName.value = "";
+    userPassword.value = "";
+  }
+});
+
+showPassword.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (userPassword.type === "password") {
+    userPassword.type = "text";
+  } else {
+    userPassword.type = "password";
+  }
+});
