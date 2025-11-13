59f91c0d6e5d02ce1dd13cc47cca3b037d6aa430
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-14 00:34:42 +0530

    solved the qestion33 js files in the in DOM questions folder of 50 questions of js

--- 50 Questions of JS/DOM Questions/question33/script32.js
+++ 50 Questions of JS/DOM Questions/question33/script32.js
@@ -1,1 +1,11 @@
 /* 33. How do you find the closest ancestor element that matches a selector?*/
+
+const usergrandParantUserName = document.querySelector("#grandParant");
+const parent = document.querySelector("parent");
+const child = document.querySelector(".child");
+
+
+
+child.addEventListener("click", (e) => { 
+  console.log(e.target.closest('.parent'));
+});
