c0dcdb741da6d371ec695447a19a590fb158966d
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-14 01:35:27 +0530

    solved the qestion34 js files in the in DOM questions folder of 50 questions of js

--- 50 Questions of JS/DOM Questions/question34/script34.js
+++ 50 Questions of JS/DOM Questions/question34/script34.js
@@ -1,2 +1,12 @@
 /* 34. How would you toggle a class on an element when another element is clicked (e.g.,
 show/hide sidebar)?*/
+
+const asideBar = document.querySelector("#aside");
+const listEL = document.querySelector(".list");
+const openCloseSideBar = document.querySelector(".sideBarbutton");
+
+openCloseSideBar.addEventListener("click", () => {
+  asideBar.classList.toggle("open");
+  listEL.classList.toggle("close");
+  openCloseSideBar.innerText = openCloseSideBar.innerText === "<" ? ">" : "<";
+});
