f22833c485a936f6973aa1a270d2a44a5f17c9ca
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-16 21:29:56 +0530

    solved the question36 with explaintion in comment js files in the in DOM questions folder of 50 questions of js

--- 50 Questions of JS/DOM Questions/question36/script36.js
+++ 50 Questions of JS/DOM Questions/question36/script36.js
@@ -1,12 +1,12 @@
 /* 35. 36. Create a Sticky Header on Scroll. The header becomes fixed to the top after scrolling
 down. (Key concepts: scroll event, window.scrollY, classList.add/remove)*/
 
-const headerEl = document.querySelector(".header");
+const headerEl = document.querySelector(".header"); // Selects the header element using its class name
 
-document.addEventListener("scroll", () => {
-  if (window.scrollY > 100) {
-    headerEl.classList.add("stick");
-  } else {
-    headerEl.classList.remove("stick");
+document.addEventListener("scroll", () => { // Adds an event listener that fires whenever the user scrolls the document
+  if (window.scrollY > 100) { // Checks if the vertical scroll position is greater than 100 pixels
+    headerEl.classList.add("stick"); // If true, adds the class 'stick' to the header element (making it sticky)
+  } else { // Executes if the scroll position is 100 pixels or less
+    headerEl.classList.remove("stick"); // Removes the class 'stick' from the header element (unsticking it)
   }
 });
