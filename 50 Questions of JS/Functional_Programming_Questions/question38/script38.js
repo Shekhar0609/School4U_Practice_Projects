84fcdbdb206b463540defbe0b1b148f5899fc4c5
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-24 00:57:54 +0530

    solved the question38 with explaintion in comment js files in the in functional programming questions folder of 50 questions of js

--- 50 Questions of JS/Functional_Programming_Questions/question38/script38.js
+++ 50 Questions of JS/Functional_Programming_Questions/question38/script38.js
@@ -1,2 +1,18 @@
 /* 38. How would you use .map() to transform a list of products into a list of HTML elements?*/
 
+const products = [
+  { id: 1, name: "Laptop", price: 1200 },
+  { id: 2, name: "Mouse", price: 25 },
+  { id: 3, name: "Keyboard", price: 75 },
+  { id: 4, name: "Pad", price: 34 },
+];
+
+const productElements = products.map((product) => {
+  return `<div class="product">
+    <h2>${product.name}</h2>
+    <p>Price: $${product.price}</p>
+  </div>`;
+});
+
+const containerElement = document.getElementById("product-container");
+containerElement.innerHTML = productElements.join("\n");
