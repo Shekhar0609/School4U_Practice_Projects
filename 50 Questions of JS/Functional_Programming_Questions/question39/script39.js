e848c0720211b74e26f71c66a61248b2153378f1
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-24 01:41:22 +0530

    solved the qestion39 js files in the in functional programming questions folder of 50 questions of js

--- 50 Questions of JS/Functional_Programming_Questions/question39/script39.js
+++ 50 Questions of JS/Functional_Programming_Questions/question39/script39.js
@@ -1,2 +1,13 @@
 /* 39. How do you use .reduce() to calculate the total price in a shopping cart?*/
 
+const shoppingCart = [
+  { item: "Laptop", price: 999.99 },
+  { item: "Phone", price: 499.99 },
+  { item: "Tablet", price: 299.99 },
+];
+
+const totalPrice = shoppingCart.reduce((acc, product) => {
+  return acc + product.price;
+}, 0);
+
+console.log(totalPrice.toFixed(2)); // Output: 1799.97
