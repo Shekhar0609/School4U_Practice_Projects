76f0acf57b2b154ca7c9170cf6c30d42743d6ba6
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-25 01:41:52 +0530

    created the question41 in the in functional programming questions folder of 50 questions of js

---
+++ 50 Questions of JS/Functional_Programming_Questions/question41/script41.js
@@ -0,0 +1,4 @@
+/* 41. How would you compose multiple functions to transform data step-by-step (e.g., sanitize → trim →
+capitalize)? Scenario: You’re preparing user input before storing it. (Expected knowledge: Function
+composition, chaining, pipe or compose logic.)*/
+
