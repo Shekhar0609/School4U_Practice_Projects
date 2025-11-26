c7a1803b66b671e5ffd07021b766f1afd0d656c6
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-27 01:31:54 +0530

    solved the qestion44 js files in the in functional programming questions folder of 50 questions of js

--- 50 Questions of JS/Dates_Questions/question45/script45.js
+++ 50 Questions of JS/Dates_Questions/question45/script45.js
@@ -1,3 +1,17 @@
 /* 45. Validate that a user's selected date range is no longer than 30 days. */
 
+function validateDateRange(userDate) {
+  const currentDate = new Date();
+  // console.log(date.toLocaleDateString());
+  const userEnteredDate = new Date(userDate);
 
+  let deffDate = (userEnteredDate - currentDate) / 1000 / 60 / 60 / 24;
+
+  if (deffDate > 30) {
+    console.log("more than 30 days");
+  } else {
+    console.log("less than 30 days");
+  }
+}
+
+validateDateRange("2025/12/01");
