4c78534d9ec786914513254d608bf4a03e469577
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-29 17:41:42 +0530

    solved the qestion47 js files in the Dates questions folder of 50 questions of js

--- 50 Questions of JS/Dates_Questions/question47/script47.js
+++ 50 Questions of JS/Dates_Questions/question47/script47.js
@@ -1,1 +1,12 @@
 /* 47. Add or subtract n days from a given date. (E.g., Add 7 days to "2025-05-01“) */
+
+const dateInput = document.getElementById("dateInput");
+const daysInput = document.getElementById("daysInput");
+const calculateBtn = document.getElementById("calculateBtn");
+const resultSpan = document.getElementById("resultSpan");
+
+calculateBtn.addEventListener("click", function () {
+  let date = new Date(dateInput.value);
+  date.setDate(date.getUTCDate() + parseInt(daysInput.value));
+  resultSpan.textContent = date.toISOString().split("T")[0];
+});
