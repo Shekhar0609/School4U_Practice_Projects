ed227dd525615e1e30b61276e8064cbe1ca23f17
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-11-30 01:33:57 +0530

    solved the qestion48 js files in the Dates questions folder of 50 questions of js

--- 50 Questions of JS/Dates_Questions/question48/script48.js
+++ 50 Questions of JS/Dates_Questions/question48/script48.js
@@ -1,1 +1,19 @@
 /* 48. Calculate the user's age from their date of birth. */
+
+const dateInput = document.getElementById("dateInput");
+const calculateBtn = document.getElementById("calculateBtn");
+const resultSpan = document.getElementById("resultSpan");
+
+calculateBtn.addEventListener("click", () => {
+  const dobvalue = dateInput.value;
+  const dob = new Date(dobvalue);
+  const currentDate = new Date();
+
+  let age = currentDate.getFullYear() - dob.getFullYear();
+  const monthDiff = currentDate.getMonth() - dob.getMonth();
+  if (monthDiff < 0 || (monthDiff === 0 && currentDate.getDate() < dob.getDate())) {
+    age--;
+  }
+  resultSpan.textContent = `Age: ${age} years`;
+
+});
