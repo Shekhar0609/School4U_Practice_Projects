bbb3aba1c1a8619afe91c4dbd762874010f940e9
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-12-22 20:00:00 +0530

    implemented the while click on add button. task will added in tge task lists and in the API as well

--- simple_to_do_list_project/practice_3/src/js/script.js
+++ simple_to_do_list_project/practice_3/src/js/script.js
@@ -12,6 +12,7 @@ addBtn.addEventListener("click", function () {
   if (taskValue !== "") {
     toDoInput.classList.remove("outline-red-500");
     toDoInput.classList.add("outline-green-500");
+    postData(taskValue);
   } else {
     toDoInput.focus();
     toDoInput.classList.add("outline-red-500");
@@ -24,95 +25,123 @@ async function fetchTasks() {
   let response = await fetch(API_URL);
   let data = await response.json();
 
-  data.forEach((taskItem) => {
-    console.log(taskItem);
+  taskListsEL.innerHTML = "";
 
-    const taskListEl = document.createElement("li");
-    taskListEl.id = "taskList";
-    taskListEl.classList.add(
-      "flex",
-      "justify-between",
-      "gap-1",
-      "w-full",
-      "h-25",
-      "mb-2",
-      "p-2",
-      "bg-gray-700",
-      "rounded-lg"
-    );
-    taskListsEL.append(taskListEl);
+  if (data) {
+    data.forEach((taskItem) => {
+      const taskListEl = document.createElement("li");
+      taskListEl.id = "taskList";
+      taskListEl.classList.add(
+        "flex",
+        "justify-between",
+        "gap-1",
+        "w-full",
+        "h-25",
+        "mb-2",
+        "p-2",
+        "bg-gray-700",
+        "rounded-lg"
+      );
+      taskListsEL.append(taskListEl);
 
-    const taskEl = document.createElement("span");
-    taskEl.id = "task";
-    taskEl.classList.add(
-      "inline-block",
-      "w-full",
-      "h-full",
-      "text-white",
-      "text-wrap",
-      "overflow-y-auto"
-    );
-    taskEl.textContent = taskItem.taskText;
-    taskListEl.append(taskEl);
-    toDoInput.value = "";
+      const taskEl = document.createElement("span");
+      taskEl.id = "task";
+      taskEl.classList.add(
+        "inline-block",
+        "w-full",
+        "h-full",
+        "text-white",
+        "text-wrap",
+        "overflow-y-auto"
+      );
+      taskEl.textContent = taskItem.taskText;
+      taskListEl.append(taskEl);
+      toDoInput.value = "";
 
-    const buttonContainerEl = document.createElement("div");
-    buttonContainerEl.id = "buttonContainer";
-    buttonContainerEl.classList.add(
-      "inline-flex",
-      "flex-col",
-      "justify-baseline",
-      "items-start",
-      "gap-3",
-      "px-1"
-    );
-    taskListEl.append(buttonContainerEl);
+      const buttonContainerEl = document.createElement("div");
+      buttonContainerEl.id = "buttonContainer";
+      buttonContainerEl.classList.add(
+        "inline-flex",
+        "flex-col",
+        "justify-baseline",
+        "items-start",
+        "gap-3",
+        "px-1"
+      );
+      taskListEl.append(buttonContainerEl);
 
-    const editBtnEl = document.createElement("button");
-    editBtnEl.id = "editBtn";
-    editBtnEl.className =
-      "bg-blue-400 hover:bg-blue-300 hover:cursor-pointer hover:scale-120 duration-300 rounded-sm p-0.5";
-    editBtnEl.innerHTML = `<i class="fas fa-edit"></i>`;
-    buttonContainerEl.append(editBtnEl);
+      const editBtnEl = document.createElement("button");
+      editBtnEl.id = "editBtn";
+      editBtnEl.className =
+        "bg-blue-400 hover:bg-blue-300 hover:cursor-pointer hover:scale-120 duration-300 rounded-sm p-0.5";
+      editBtnEl.innerHTML = `<i class="fas fa-edit"></i>`;
+      buttonContainerEl.append(editBtnEl);
 
-    const trashEl = document.createElement("button");
-    trashEl.id = "trashEl";
-    trashEl.className =
-      "bg-red-400 hover:bg-red-300 hover:cursor-pointer hover:scale-120 duration-300 rounded-sm p-0.5";
-    trashEl.innerHTML = `<i class="fas fa-trash-alt"></i>`;
-    buttonContainerEl.append(trashEl);
+      const trashEl = document.createElement("button");
+      trashEl.id = "trashEl";
+      trashEl.className =
+        "bg-red-400 hover:bg-red-300 hover:cursor-pointer hover:scale-120 duration-300 rounded-sm p-0.5";
+      trashEl.innerHTML = `<i class="fas fa-trash-alt"></i>`;
+      buttonContainerEl.append(trashEl);
 
-    function editAndCheckTask() {
-      const isDisabled = taskEl.hasAttribute("contenteditable");
-      if (isDisabled) {
-        taskEl.removeAttribute("contenteditable");
-        editBtnEl.innerHTML = `<i class="fas fa-edit"></i>`;
-        editBtnEl.classList.remove(
-          "text-green-400",
-          "hover:bg-green-500",
-          "outline-green",
-          "hover:text-green-200"
-        );
-        editBtnEl.classList.add("bg-blue-400", "hover:bg-blue-300");
-        console.log(editBtnEl);
-      } else {
-        taskEl.setAttribute("contenteditable", "true");
-        taskEl.focus();
-        editBtnEl.innerHTML = `<i class="fas fa-check-circle"></i>`;
-        editBtnEl.classList.remove("bg-blue-400", "hover:bg-blue-300");
-        editBtnEl.classList.add(
-          "text-green-400",
-          "hover:bg-green-500",
-          "outline-green",
-          "hover:text-green-200"
-        );
+      function editAndCheckTask() {
+        const isDisabled = taskEl.hasAttribute("contenteditable");
+        if (isDisabled) {
+          taskEl.removeAttribute("contenteditable");
+          editBtnEl.innerHTML = `<i class="fas fa-edit"></i>`;
+          editBtnEl.classList.remove(
+            "text-green-400",
+            "hover:bg-green-500",
+            "outline-green",
+            "hover:text-green-200"
+          );
+          editBtnEl.classList.add("bg-blue-400", "hover:bg-blue-300");
+          console.log(editBtnEl);
+        } else {
+          taskEl.setAttribute("contenteditable", "true");
+          taskEl.focus();
+          editBtnEl.innerHTML = `<i class="fas fa-check-circle"></i>`;
+          editBtnEl.classList.remove("bg-blue-400", "hover:bg-blue-300");
+          editBtnEl.classList.add(
+            "text-green-400",
+            "hover:bg-green-500",
+            "outline-green",
+            "hover:text-green-200"
+          );
+        }
       }
-    }
 
-    trashEl.addEventListener("click", () => {
-      taskListEl.remove();
+      trashEl.addEventListener("click", () => {
+        taskListEl.remove();
+      });
+
+      editBtnEl.addEventListener("click", editAndCheckTask);
     });
+  } else {
+    const noTaskEl = document.createElement("p");
+    noTaskEl.classList.add("text-white", "text-center", "mt-4");
+    noTaskEl.textContent = "No Tasks Available. Please add a task.";
+    mainEl.append(noTaskEl);
+  }
+}
 
-    editBtnEl.addEventListener("click", editAndCheckTask);
+async function postData(taskValue) {
+  let taskData = {
+    taskText: taskValue,
+  };
+
+  let response = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(taskData),
   });
+
+  let data = await response.json();
+  if (response.ok) {
+    fetchTasks();
+  } else {
+    alert("Error adding task: " + data.message);
+  }
 }
