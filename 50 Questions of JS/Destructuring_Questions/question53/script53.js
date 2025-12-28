aacd4e29112e6eef59ff63b36705e00f7dbd09ae
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-12-28 23:57:57 +0530

    solved the question53 file in the Destructuring questions folder of 53 questions of js

--- 50 Questions of JS/Destructuring_Questions/question53/script53.js
+++ 50 Questions of JS/Destructuring_Questions/question53/script53.js
@@ -1,3 +1,19 @@
 /* 53. Write a function that take numbers as argument and separates even and odd numbers and return
 an object with evens and odds and destructure the output while calling function. */
 
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function oddEvenNumbers(...nums) {
+  let odd = [];
+  let even = [];
+  nums.filter((num) => {
+    num % 2 !== 0 ? odd.push(num) : even.push(num);
+  });
+
+  return { odd, even };
+}
+
+let { odd, even } = oddEvenNumbers(...nums);
+
+console.log("Odd Numbers are: ", odd);
+console.log("Even Numbers are: ", even);
