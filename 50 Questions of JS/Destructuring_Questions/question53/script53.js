579b6fd4ed31d421c7dccfb070a430a6ad44f967
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-12-28 23:31:07 +0530

    created the question53 in the in Destructuring questions folder of 53 questions of js

---
+++ 50 Questions of JS/Destructuring_Questions/question53/script53.js
@@ -0,0 +1,3 @@
+/* 53. Write a function that take numbers as argument and separates even and odd numbers and return
+an object with evens and odds and destructure the output while calling function. */
+
