8213bf1a3d8cd4300587a19baf17eb89263d2f28
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-12-29 00:15:09 +0530

    solved the question54 file in the Destructuring questions folder of 54 questions of js

--- 50 Questions of JS/Destructuring_Questions/question54/script54.js
+++ 50 Questions of JS/Destructuring_Questions/question54/script54.js
@@ -4,4 +4,19 @@ o Store a value (like state).
 o Return two things: the current value and a function to update it.
 Use array destructuring to extract both the value and the setter when calling your function. */
 
-// Solution: 
+// Solution :
+function useState(initialValue) {
+  let value = initialValue;
+  function setValue(newValue) {
+    value = newValue;
+    console.log("Value updated to:", value);
+  }
+  return [() => value, setValue];
+}
+const [getValue, setValue] = useState(5);
+console.log(getValue());
+setValue(15);
+console.log(getValue());
+setValue(25);
+console.log(getValue());
+
