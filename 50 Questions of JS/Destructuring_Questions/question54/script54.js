a52beb74d1f2cdc236fdac987849f0f51a7c79b6
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-12-29 00:02:25 +0530

    created the question54 in the in Destructuring questions folder of 54 questions of js

---
+++ 50 Questions of JS/Destructuring_Questions/question54/script54.js
@@ -0,0 +1,7 @@
+/* 54. Create a custom JavaScript function that behaves like React’s useState.
+The function should:
+o Store a value (like state).
+o Return two things: the current value and a function to update it.
+Use array destructuring to extract both the value and the setter when calling your function. */
+
+// Solution: 
