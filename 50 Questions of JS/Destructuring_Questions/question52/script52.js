3fde625f1127add882c4053cacf066d14a15c41f
Shekhar0609 <shekhar.deekonda0609@gmail.com>
2025-12-28 23:29:52 +0530

    solved the question52 with explaintion in comment js files in the Destructuring questions folder of 52 questions of js

--- 50 Questions of JS/Destructuring_Questions/question52/script52.js
+++ 50 Questions of JS/Destructuring_Questions/question52/script52.js
@@ -1,16 +1,22 @@
-/* 2. How does using spread help avoid mutation? Modify the object without affecting the original? */
+/* 52. How does using spread help avoid mutation? Modify the object without affecting the original? */
 
+
+// Original Object
 const originalObj = {
   fname: "shekhar",
   age: 28,
 };
 
+
+// Using Spread Operator to create a copy and modify it without affecting the original object
 const copyOriginalObj = { ...originalObj, contactNo: 12345321 };
 
+// Modifying nested object to demonstrate deep mutation avoidance
 copyOriginalObj.address = {
   city: "mumbai",
   state: "maharashtra",
 };
 
-console.log(originalObj);
-console.log(copyOriginalObj);
+
+console.log(originalObj); // Original object remains unchanged
+console.log(copyOriginalObj);  // New object with added properties
